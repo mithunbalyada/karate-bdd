aa55185eb7e160f68fe7de7675e4aa20a0a1a542
Mithun <mithunbalyada@gmail.com>
2019-03-20 02:11:47 +0000

    migrating from gradle to maven

--- src/test/java/com/mithun/gfk/myapi/TestRunner.java
+++ src/test/java/com/mithun/gfk/myapi/TestRunner.java
@@ -1,27 +1,32 @@
 package com.mithun.gfk.myapi;
 
+
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import com.intuit.karate.formats.selenium.TestBase;
 import com.intuit.karate.junit4.Karate;
-import cucumber.api.CucumberOptions;
 import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import static org.junit.Assert.assertTrue;
 
-@RunWith(Karate.class)
-//@KarateOptions(features = "classpath:features/")
-@CucumberOptions(features = "classpath:features/")
+/**
+ * <p>
+ *     Karate test launcher
+ * </p>
+ */
+
+@KarateOptions(
+        features = "classpath:features/"
+)
 public class TestRunner {
 
     @BeforeClass
@@ -29,19 +34,18 @@ public class TestRunner {
 
     }
 
+
     @Test
     public void testParallel() {
-        System.setProperty("karate.env", "dev"); // ensure reset if other tests (e.g. mock) had set env in CI
-        Results results = Runner.parallel(getClass(), 5);
-        generateReport(results.getReportDir());
+        Results results = Runner.parallel(getClass(), 5, "target/cucumber-report");
         assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
     }
 
     public static void generateReport(String karateOutputPath) {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "my-api-test");
+        Configuration config = new Configuration(new File("target"), "demo");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
